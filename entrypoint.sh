b2ab030ad5db06d334368bc516778be6876c5f73
Thomas Piccirello <thomas@piccirello.com>
2023-12-15 10:55:23 -0800

    Support graceful shutdown

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+handler() {
+    /usr/sbin/postfix stop
+    exit
+}
+
+trap handler INT
+trap handler TERM
+
 # start syslog so postfix writes logs
 syslog-ng
 
@@ -11,4 +19,6 @@ postmap sasl_passwd
 /usr/sbin/postfix start
 
 # print postfix logs to stdout
-tail -f /var/log/mail.log
+# we send this to the background so the script's traps run
+tail -f /var/log/mail.log &
+wait
