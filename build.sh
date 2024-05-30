6ee4bf5171aa96082e1e761ade59a1195baeac99
Thomas Piccirello <thomas@piccirello.com>
2024-05-30 13:25:58 -0700

    Add support for arm64

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-docker build --push -t piccirello/postfix -f Dockerfile --platform linux/amd64 .
+docker build --push -t piccirello/postfix -f Dockerfile --platform "linux/amd64,linux/arm64" .
